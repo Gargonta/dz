5ed634bbd34eaa1d664bda44c9886de5c8fb026d
maddox <arahnarok@gmail.com>
2021-10-19 00:44:40 +0300

    2ое дз не полное

--- src/App.js
+++ src/App.js
@@ -1,130 +1,68 @@
-import React from 'react';
+import React from 'react'
 
-import './App.scss';
+import './App.scss'
+import Item from "./components/Item/Item"
+import ItemList from "./components/ItemList/ItemList"
 
 function App() {
+  const content = [
+    {
+      date: '03.06.2020',
+      site: 'utinet.ru',
+      text: '<p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>',
+      status: 'Открыть согласование',
+      statusId: 1,
+    },
+    {
+      date: '03.06.2020',
+      site: 'utinet.ru',
+      text: '<p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>',
+      status: 'Создана',
+      statusId: 2,
+    },
+    {
+      date: '03.06.2020',
+      site: 'utinet.ru',
+      text: '<p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>',
+      status: 'Ожидает согласования',
+      statusId: 3,
+    },
+    {
+      date: '03.06.2020',
+      site: 'utinet.ru',
+      text: '<p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>',
+      status: 'Отклонено',
+      statusId: 4,
+    },
+    {
+      date: '03.06.2020',
+      site: 'utinet.ru',
+      text: '<p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>',
+      status: 'На размещении',
+      statusId: 5,
+    },
+  ]
+
   return (
     <div className="container">
-    <div className="wrap-input-item item_1">
-      <div className="wrap-input-check-wrap">
-        <input type="checkbox" name="form-check_input" id="form-check_input_1" className="form-check_input" value="Y"/>
-        <label className="form-check" for="form-check_input_1">
-          <span>03.06.2020</span>
-        </label>
-      </div>
-      <div className="wrap-email-block">
-        <a href="#">utinet.ru</a>
-      </div>
-      <div className="wrap-text-block">
-        <p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом, диалогом и полилогом. В каждом из них она представляет либо идеологическую, либо коммуникативную, либо смысловую направленность, в зависимости от авторского замысла.</p>
-      </div>
-      <div className="wrap-status-block">
-        <div className="icon-block"><svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4.67497 17.649C4.90024 17.8737 5.20593 18 5.5247 18C5.84347 18 6.14916 17.8737 6.37443 17.649L10.7364 13.2912C11.1191 12.815 11.0812 12.1282 10.6482 11.6968C10.2153 11.2654 9.52607 11.2276 9.04824 11.609L6.7823 13.9234L6.7823 1.3128C6.82663 0.859591 6.609 0.420579 6.22085 0.180248C5.83271 -0.0600828 5.34132 -0.0600828 4.95317 0.180248C4.56503 0.420579 4.3474 0.859591 4.39173 1.3128L4.39173 13.9234L2.12579 11.6654C1.67527 11.1666 0.904254 11.1262 0.403676 11.5751C-0.0969013 12.024 -0.137481 12.7923 0.313039 13.2912L4.67497 17.649Z" fill="white"/>
-        </svg>
-      </div>
-      <p>Открыть согласование</p>
+  {/*   {content && content.length ? content.map((item, index) => (
+        <Item
+          key={index}
+          date={item.date}
+          site={item.site}
+          text={item.text}
+          status={item.status}
+          statusId={item.statusId}
+        />
+      )) : null} 
+  */}
+  <ItemList/>
+  <ItemList/>
+  <ItemList/>
+  <ItemList/>
+  <ItemList/>
     </div>
-  </div>
-  <div className="wrap-input-item item_2">
-    <div className="wrap-input-check-wrap">
-      <input type="checkbox" name="form-check_input" id="form-check_input_2" className="form-check_input" value="Y"/>
-      <label className="form-check" for="form-check_input_2">
-        <span>03.06.2020</span>
-      </label>
-    </div>
-    <div className="wrap-email-block">
-      <a href="#">utinet.ru</a>
-    </div>
-    <div className="wrap-text-block">
-      <p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом.</p>
-      <a href="#">
-        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7029 11.602L16.4915 8.81403C18.5029 6.79573 18.4997 3.52973 16.4843 1.5154C14.4689 -0.498933 11.2029 -0.500441 9.18567 1.51203L6.39707 4.30003C6.38506 4.31203 6.37505 4.32203 6.36507 4.33403L7.63739 5.60603C7.64739 5.59403 7.65739 5.58403 7.66939 5.57203L10.458 2.78403C11.7719 1.46981 13.9024 1.46954 15.2166 2.78343C16.5308 4.09731 16.5311 6.22781 15.2172 7.54203L12.4286 10.33C12.4166 10.342 12.4066 10.352 12.3946 10.362L13.6689 11.636L13.7029 11.602ZM5.16464 18C6.53563 18.0036 7.85103 17.4584 8.81755 16.486L11.6061 13.698C11.6182 13.686 11.6282 13.676 11.6382 13.664L10.3659 12.392L7.54325 15.214C6.2288 16.5249 4.101 16.5236 2.78815 15.2111C1.4753 13.8986 1.47347 11.7708 2.78405 10.456L5.57465 7.66803L5.5747 7.66798L5.57471 7.66797C5.5867 7.656 5.59669 7.64601 5.60865 7.63603L4.33434 6.36203L1.51174 9.18403C0.0353311 10.6614 -0.405971 12.8826 0.393542 14.8121C1.19306 16.7417 3.076 17.9998 5.16464 18ZM11.9382 4.79083C12.2897 4.4394 12.8596 4.4394 13.2111 4.79083C13.3799 4.95957 13.4748 5.18847 13.4748 5.42715C13.4748 5.66583 13.3799 5.89473 13.2111 6.06348L6.06362 13.2092C5.71219 13.5607 5.14233 13.5608 4.79082 13.2093C4.4393 12.8579 4.43924 12.288 4.79067 11.9365L11.9382 4.79083Z" fill="#94A2B6"/>
-        </svg>
-      utinet.ru</a>
-    </div>
-    <div className="wrap-status-block">
-      <div className="icon-block">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 18.6274 5.37258 24 12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM7 10.9394H10.9394V7H13.0606V10.9394H17V13.0606H13.0606V17H10.9394V13.0606H7V10.9394Z" fill="#FF4A00"/>
-        </svg>
-
-      </div>
-      <p>Создана</p>
-    </div>
-  </div>
-  <div className="wrap-input-item item_3">
-    <div className="wrap-input-check-wrap">
-      <input type="checkbox" name="form-check_input" id="form-check_input_3" className="form-check_input" value="Y"/>
-      <label className="form-check" for="form-check_input_3">
-        <span>03.06.2020</span>
-      </label>
-    </div>
-    <div className="wrap-email-block">
-      <a href="#">utinet.ru</a>
-    </div>
-    <div className="wrap-text-block">
-      <p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом, диалогом и полилогом. В каждом из них она представляет либо идеологическую, либо коммуникативную, либо смысловую направленность, в зависимости от авторского замысла.</p>
-    </div>
-    <div className="wrap-status-block">
-      <div className="icon-block">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM14.23 15.7L11.3 12.761C11.0935 12.5581 10.9842 12.2761 11 11.987V7C11 6.44772 11.4477 6 12 6C12.5523 6 13 6.44772 13 7V11.531L15.7 14.231C16.1057 14.6369 16.1054 15.2948 15.6995 15.7005C15.2936 16.1062 14.6357 16.1059 14.23 15.7Z" fill="#2E2382"/>
-        </svg>
-
-      </div>
-      <p>Ожидает согласования</p>
-    </div>
-  </div>
-  <div className="wrap-input-item item_4">
-    <div className="wrap-input-check-wrap">
-      <input type="checkbox" name="form-check_input" id="form-check_input_4" className="form-check_input" value="Y"/>
-      <label className="form-check" for="form-check_input_4">
-        <span>03.06.2020</span>
-      </label>
-    </div>
-    <div className="wrap-email-block">
-      <a href="#">utinet.ru</a>
-    </div>
-    <div className="wrap-text-block">
-      <p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом, диалогом и полилогом.</p>
-    </div>
-    <div className="wrap-status-block">
-      <div className="icon-block">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M0 12C0 5.37258 5.37258 0 12 0C18.6242 0.00773676 23.9923 5.37579 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM2.4 12C2.4 17.3019 6.69807 21.6 12 21.6C17.2994 21.5939 21.5939 17.2994 21.6 12C21.6 6.69807 17.3019 2.4 12 2.4C6.69807 2.4 2.4 6.69807 2.4 12Z" fill="#E00C3C"/>
-        </svg>
-
-      </div>
-      <p>Отклонено</p>
-    </div>
-  </div>
-  <div className="wrap-input-item item_5">
-    <div className="wrap-input-check-wrap">
-      <input type="checkbox" name="form-check_input" id="form-check_input_5" className="form-check_input" value="Y"/>
-      <label className="form-check" for="form-check_input_5">
-        <span>03.06.2020</span>
-      </label>
-    </div>
-    <div className="wrap-email-block">
-      <a href="#">utinet.ru</a>
-    </div>
-    <div className="wrap-text-block">
-      <p>Статья рассматривает такую структурную и семантическую единицу драматургического текста, как реплика, которая реализуется, будучи связанной с тремя другими компонентами текста: монологом, диалогом и полилогом. В каждом из них она представляет либо идеологическую, либо коммуникативную, либо смысловую направленность, в зависимости от авторского замысла.</p>
-    </div>
-    <div className="wrap-status-block">
-      <div className="icon-block">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24C18.6274 24 24 18.6274 24 12C23.9923 5.37579 18.6242 0.00773676 12 0ZM12 21.6C6.69807 21.6 2.4 17.3019 2.4 12C2.4 6.69807 6.69807 2.4 12 2.4C17.3019 2.4 21.6 6.69807 21.6 12C21.5939 17.2994 17.2994 21.5939 12 21.6ZM16.8 10.0969L15.1031 8.4L10.8 12.7031L8.89692 10.8L7.2 12.4969L9.9516 15.2484C10.1766 15.4735 10.4818 15.6 10.8 15.6C11.1182 15.6 11.4234 15.4735 11.6484 15.2484L16.8 10.0969Z" fill="#119E00"/>
-        </svg>
-
-      </div>
-      <p>На размещении</p>
-    </div>
-  </div>
-</div>
-  );
+  )
 }
 
 export default App;
